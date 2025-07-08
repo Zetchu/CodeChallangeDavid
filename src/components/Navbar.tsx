08b9cfb6b7e5197514d747bdc335d23c72638b78
Zetchu <shmungula@gmail.com>
2025-07-08 19:47:01 +0200

    refactor: improve Hero section layout and enhance Navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,27 @@
-// src/components/Navbar.tsx
-// src/components/Navbar.tsx
+import hamburgerIcon from '../assets/hamburger.svg';
+
 const Navbar = () => {
   return (
-    <nav className='absolute top-0 left-0 w-full bg-[#6C63FF] text-white px-6 py-4 flex justify-between items-center z-50'>
+    <nav className='absolute top-0 left-0 w-full bg-[#685DC5] text-white px-6 py-4 flex justify-between items-center z-50'>
       <div className='text-sm font-bold'>
         HARBOUR.SPACE <span className='font-normal'>/INTERACTION DESIGN</span>
       </div>
 
-      <button className='bg-green-500 text-white font-bold rounded-full px-5 py-2 text-xs hover:bg-green-600 transition'>
-        APPLY NOW
-      </button>
+      <div className='relative'>
+        <button
+          className='w-20 h-20  lg:display-block bg-green-500 text-white font-bold rounded-full text-xs hover:bg-green-600 transition shadow-lg
+                     hidden lg:flex items-center justify-center absolute -top-2 right-20'
+        >
+          APPLY
+          <br />
+          NOW
+        </button>
+        <img
+          src={hamburgerIcon}
+          alt='Menu'
+          className='w-10 h-10 cursor-pointer'
+        />
+      </div>
     </nav>
   );
 };
